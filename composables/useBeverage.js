e3dd96d261b99291fb2ac457c81c100432a57410
concept254 <concept254@gmail.com>
2024-12-31 13:26:20 +0200

    Use beverage composable

---
+++ composables/useBeverage.js
@@ -0,0 +1,7 @@
+export const useBeverage = () => {
+    return [
+        { id: 1, name: "Beer", price: 45.00 },
+        { id: 2, name: "Cider", price: 52.00 },
+        { id: 3, name: "Premix", price: 59.00 },
+    ];
+};
